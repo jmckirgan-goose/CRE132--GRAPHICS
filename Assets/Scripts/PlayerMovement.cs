4679c1ff792e31ce5b1d56680448c1affffa5641
jmckirgan-goose <mckirgan-j@ulster.ac.uk>
2023-11-14 15:13:40 +0000

    Update 2
    Character animated, camera following player and level design updated

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Unity.VisualScripting.Dependencies.Sqlite;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour {
 
     private Vector2 movement;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Awake() {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    private void OnMovement (InputValue value) {
+    private void OnMovement(InputValue value) {
         movement = value.Get<Vector2>();
-        
-    }
 
-    private void FixedUpdate() {
+        if (movement.x != 0 || movement.y != 0) {
+
+
+            animator.SetFloat("X", movement.x);
+            animator.SetFloat("Y", movement.y);
+
+            animator.SetBool("IsWalking", true);
+        } else {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+    private void FixedUpdate()
+    {
+        Debug.Log(movement);
         float speed = 3.0f;
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
+    
 }
