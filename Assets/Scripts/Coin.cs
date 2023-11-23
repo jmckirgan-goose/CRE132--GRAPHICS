ae34f20f7cc8ff92352a76c9cd420dee99068bac
jmckirgan-goose <mckirgan-j@ulster.ac.uk>
2023-11-23 10:30:14 +0000

    Update 5
    Layers, coins, sfx and new scene for battle system

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private int value;
+    private bool hasTriggered;
+
+    private CoinManager coinManager;
+
+    private void Start()
+    {
+        coinManager = CoinManager.instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if (collision.CompareTag("Player"))
+        {
+            
+            coinManager.ChangeCoins(value);
+            Destroy(gameObject);
+        } 
+    }
+
+
+}
---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using TMPro;
+
+public class CoinManager : MonoBehaviour
+{
+    public static CoinManager instance;
+
+    private int coins;
+    [SerializeField] private TMP_Text coinsDisplay;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+
+    }
+
+    private void Update()
+    {
+        coinsDisplay.text = coins.ToString();
+    }
+
+
+    public void ChangeCoins(int amount)
+    {
+        coins += amount;
+    }
+
+
+
+
+}
