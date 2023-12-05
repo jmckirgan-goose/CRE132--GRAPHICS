6adcfdd22e90fc1cc1bd7767149f0a280c4a7cef
jmckirgan-goose <mckirgan-j@ulster.ac.uk>
2023-12-05 15:17:33 +0000

    Update 11
    Huds fixed

--- Assets/BattleHuds.cs
+++ Assets/BattleHuds.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BattleHuds : MonoBehaviour
 {
-    public Text nameText;
+    public TMP_Text nameText;
     public Text levelText;
     public Slider hpSlider;
 
